206c887b019f5d93672cddc2a314b4dde5302ca1
Yeboster <yeboster@gmail.com>
2020-03-26 19:02:28 +0100

    Switch hugo to extended (scss support) and update it to 0.68.3.

--- nix/hugo.nix
+++ nix/hugo.nix
@@ -6,8 +6,8 @@ in
 stdenv.mkDerivation {
   name = "hugo-0.67";
   src = fetchurl {
-    url = https://github.com/gohugoio/hugo/releases/download/v0.67.1/hugo_0.67.1_Linux-64bit.tar.gz;
-    sha256 = "3076ecd5426f4f1e46a4ca4242bde22c9a137199e5ab93fbbefb0271d25bc52a";
+    url = https://github.com/gohugoio/hugo/releases/download/v0.68.3/hugo_extended_0.68.3_Linux-64bit.tar.gz;
+    sha256 = "d93d0deac782a4dd8afc2bbc5d96b30590ce47e8afb9810bbe7551eb3acf9189";
   };
 
   # Since hugo tarball hasn't a folder, I need to unpack it myself
