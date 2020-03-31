5235beee084d92190a47de4529d849ae23108978
Yeboster <admin@yeboster.club>
2020-03-31 14:31:56 +0000

    Nix: Reflect hugo package name to its version.

--- nix/hugo.nix
+++ nix/hugo.nix
@@ -4,7 +4,7 @@ let
   fetchurl = pkgs.fetchurl;
 in
 stdenv.mkDerivation {
-  name = "hugo-0.67";
+  name = "hugo-0.68.3";
   src = fetchurl {
     url = https://github.com/gohugoio/hugo/releases/download/v0.68.3/hugo_extended_0.68.3_Linux-64bit.tar.gz;
     sha256 = "d93d0deac782a4dd8afc2bbc5d96b30590ce47e8afb9810bbe7551eb3acf9189";
